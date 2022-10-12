d2dbab3316df7fd85ecc52f8dfa3e13e945af926
Aleksandra <alexandra.bakhcheva@gmail.com>
2022-10-12 22:40:45 +0300

    changed to map rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from "react";
-import Tariff from "./assets/components/tariff";
+import tariffs from "./assets/tariffs-array";
+import Tariff from "./assets/components/Tariff";
 import "./assets/styles/index.css"
 
 function App() {
-  const isSelected = true;
+
+  let isSelected;
+
   return (
     <div className="container">
-      <div className="tariff tariff__300">
-        <Tariff amount={300} option={10}></Tariff>
-      </div>
-      <div className="tariff tariff__450">
-        <Tariff amount={450} option={50}></Tariff>
-      </div>
-      <div className={"tariff tariff__550 " + (isSelected ? "selected" : "")}>
-        <Tariff amount={550} option={100}></Tariff>
-      </div>
-      <div className="tariff tariff__1000">
-        <Tariff amount={1000} option={200}></Tariff>
-      </div>
+      {
+        tariffs.map((tariff, index) =>
+        <div className={"tariff tariff__" + `${index}` + (isSelected ? " selected" : "")}>
+          <Tariff key={tariff} header={tariff.header} currency={tariff.currency} amount={tariff.amount} price={tariff.price} option={tariff.option} note={tariff.note} isSelected={tariff.isSelected} />
+        </div>
+        )
+      }
     </div>
   );
 }
--- src/assets/components/tariff.jsx
+++ src/assets/components/tariff.jsx
@@ -3,18 +3,15 @@ import "../styles/index.css";
 function Tariff(props) {
 
     return (
-        <div>
-            <h2 className="tariff__header">{`Безлимитный ${props.amount}`}</h2>
+        <div>{props.isSelected}
+            <h2 className="tariff__header">{props.header}</h2>
             <div className="tariff__price">
-                <p className="tariff__currency">{"руб"}</p>
+                <p className="tariff__currency">{props.currency}</p>
                 <div className="tariff__amount">{props.amount}</div>
-                <div className="tariff__period">{`/мес`}</div>
+                <div className="tariff__period">{props.price}</div>
             </div>
-            <div className="tariff__option">{`до ${props.option} Мбит/сек`}</div>
-            <div className="tariff__note">
-                <p>{`Объем включенного`}</p>
-                <p>{`трафика не ограничен`}</p>
-            </div>
+            <div className="tariff__option">{props.option}</div>
+            <div className="tariff__note">{props.note}</div>
         </div >
     );
 }
